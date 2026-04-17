aed6fdf4dab12f041c93bf12e5c19069fe268297
Abhishek752-T <abhishektripathi752.com>
2026-04-17 18:21:10 +0530

    initial commit

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,10 @@
 # Print the multiplication table of 17
 for i in range(1, 13):
     print(f"17 x {i} = {17 * i}")
+
+print("\nHello, World! Abhishek" )
+print("Welcome to the world of AI and programming!")
+
+
+
+
